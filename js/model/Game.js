01eeb36f73064188b734e981fd03aefbe5a9b7e2
Scheuer Kévin <scheuer.kevin@gmail.com>
2021-07-16 17:46:47 +0200

    Adding map function generation with tileManager

--- js/model/Game.js
+++ js/model/Game.js
@@ -1,13 +1,16 @@
 import Map from './Map.js';
 import Canvas from './Canvas.js';
+import TileManager from './TileManager.js';
 
 
 export default class Game {
     constructor() {
-        this.canvas = new Canvas(document.getElementById("static-canvas"));
+        this.canvas = new Canvas(document.querySelector("#static-canvas"));
     }
 
     async init() {
+        this.tileManager = new TileManager();
+        await this.tileManager.loadFile("tiles2", 5, 1);
         this.map = new Map();
         await this.map.loadMap('map1');
         this.canvas.setStep(this.map.size);
--- js/model/Square.js
+++ js/model/Square.js
@@ -2,5 +2,29 @@ export default class Square {
     constructor(x,y) {
         this.x = x;
         this.y = y;
+
+        switch (Math.floor(Math.random()*10)) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                this.tile = "GRASS";
+                break;
+            case 5:
+            case 6:
+            case 7:
+                this.tile = "GROUND";
+                break;
+            case 8:
+                this.tile = "TREE";
+                break;
+            case 9:
+                this.tile = "BUSH";
+                break;
+        }
+
     }
+
+
 }
